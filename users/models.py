7458900f7e0a2158959300931e10dfefdedb142e
svictor <victorshayo92@gmail.com>
2025-07-30 03:02:30 +0300

    Update User model and remove Company/Customer models

--- users/models.py
+++ users/models.py
@@ -16,39 +16,3 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
-    
-    
-
-class Customer(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE,primary_key=True)
-    date_of_birth = models.DateField()
-
-    def __str__(self):
-        return self.user.username
-    
-
-
-class Company(models.Model):
-    user = models.OneToOneField(
-        User, on_delete=models.CASCADE, primary_key=True)
-    field = models.CharField(max_length=70, choices=(('Air Conditioner', 'Air Conditioner'),
-                                                     ('All in One', 'All in One'),
-                                                     ('Carpentry', 'Carpentry'),
-                                                     ('Electricity',
-                                                      'Electricity'),
-                                                     ('Gardening', 'Gardening'),
-                                                     ('Home Machines',
-                                                      'Home Machines'),
-                                                     ('House Keeping',
-                                                      'House Keeping'),
-                                                     ('Interior Design',
-                                                      'Interior Design'),
-                                                     ('Locks', 'Locks'),
-                                                     ('Painting', 'Painting'),
-                                                     ('Plumbing', 'Plumbing'),
-                                                     ('Water Heaters', 'Water Heaters')), blank=False, null=False)
-    rating = models.IntegerField(
-        validators=[MaxValueValidator(5), MinValueValidator(0)], default=0)
-
-    def __str__(self):
-        return str(self.user.id) + ' - ' + self.user.username
