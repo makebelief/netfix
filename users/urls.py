45c76b12ef011421c3a637525117f2308008c5f3
svictor <victorshayo92@gmail.com>
2025-08-03 09:12:55 +0300

    Update user URL patterns

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-from . import views
+from django.contrib.auth import views as auth_views
 
-app_name = 'users'
+
+from .forms import UserLoginForm
+from . import views as v
+app_name = "users"
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('profile/', views.profile, name='profile'),
+    path('', v.register, name='register'),
+    path('company/', v.CompanySignUpView.as_view(), name='register_company'),
+    path('customer/', v.CustomerSignUpView.as_view(), name='register_customer'),
+    path('login/', v.LoginUserView, name='login_user'),
+    path('profile/', v.profile, name='profile'),
 ]
