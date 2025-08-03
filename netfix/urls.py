6963fcb78cd03cb91b0913a1d7c69a3b7149ec63
svictor <victorshayo92@gmail.com>
2025-08-03 09:10:42 +0300

    Update URL routing configuration

--- netfix/urls.py
+++ netfix/urls.py
@@ -14,10 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+
+from . import views as v
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
+    path('', include('main.urls')),
     path('services/', include('services.urls')),
+    path('register/', include('users.urls')),
+    
+
+   path('customer/<slug:name>', v.customer_profile, name='customer_profile'),
+    
+    path('company/<slug:name>', v.company_profile, name='company_profile')
 ]
