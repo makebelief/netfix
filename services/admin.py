76c4fb2ad583baa7a6099638fbc973cbfcb71cb0
svictor <victorshayo92@gmail.com>
2025-08-03 09:11:50 +0300

    Update services admin configuration

--- services/admin.py
+++ services/admin.py
@@ -1,14 +1,8 @@
 from django.contrib import admin
-from .models import Service, RequestService
+
+from .models import Service
+
 
 @admin.register(Service)
 class ServiceAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'is_active', 'created_at')
-    list_filter = ('is_active',)
-    search_fields = ('name', 'description')
-
-@admin.register(RequestService)
-class RequestServiceAdmin(admin.ModelAdmin):
-    list_display = ('user', 'service', 'status', 'requested_date')
-    list_filter = ('status',)
-    search_fields = ('user__username', 'service__name')
+    list_display = ("id", "name", "price_hour", "field", "date")
